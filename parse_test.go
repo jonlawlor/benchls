c9202af90c5772c1e8d5e93f05d6d9d4e8028839
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2016-02-22 21:58:08 -0500

    test more of the parser

--- parse_test.go
+++ parse_test.go
@@ -14,17 +14,39 @@ func TestParse(t *testing.T) {
 	for i, tt := range []struct {
 		inre           string
 		xtrans, ytrans string
+		xrpn           [][]string
+		xstrings       []string
 		vars           map[string]float64
 		wantX          []float64
 		wantY          float64
 	}{
 		{
-			inre:   `(?P<N>\d+)-\d+$`,
-			xtrans: "math.Log(N), 1.0",
-			ytrans: "Y",
-			vars:   map[string]float64{"N": 10.0, "Y": 2.0},
-			wantX:  []float64{math.Log(10.0), 1.0},
-			wantY:  2.0,
+			inre:     `(?P<N>\d+)-\d+$`,
+			xtrans:   "math.Log(N), 1.0",
+			xrpn:     [][]string{{"N", "math.Log"}, {"1.0"}},
+			xstrings: []string{"math.Log(N)", "1.0"},
+			ytrans:   "Y",
+			vars:     map[string]float64{"N": 10.0, "Y": 2.0},
+			wantX:    []float64{math.Log(10.0), 1.0},
+			wantY:    2.0,
+		}, {
+			inre:     `(?P<N>\d+)-\d+$`,
+			xtrans:   "N*N, N, 1.0",
+			xrpn:     [][]string{{"N", "N", "*"}, {"N"}, {"1.0"}},
+			xstrings: []string{"N*N", "N", "1.0"},
+			ytrans:   "Y+1",
+			vars:     map[string]float64{"N": 10.0, "Y": 2.0},
+			wantX:    []float64{100.0, 10.0, 1.0},
+			wantY:    3.0,
+		}, {
+			inre:     `(?P<M>\d+)(?P<N>\d+)-\d+$`,
+			xtrans:   "-math.Hypot(M+N, M-N), +M/N",
+			xrpn:     [][]string{{"M", "N", "+", "M", "N", "-", "math.Hypot", "u-"}, {"M", "u+", "N", "/"}},
+			xstrings: []string{"-math.Hypot(M+N, M-N)", "+M/N"},
+			ytrans:   "Y+1",
+			vars:     map[string]float64{"M": 3.5, "N": 0.5, "Y": -1.0},
+			wantX:    []float64{-5.0, 7.0},
+			wantY:    0.0,
 		},
 	} {
 		inre := regexp.MustCompile(tt.inre)
@@ -36,6 +58,14 @@ func TestParse(t *testing.T) {
 		for xi, expr := range xExpr {
 			if x := expr.value(tt.vars); x != tt.wantX[xi] {
 				t.Errorf("%d: expected x[%d] = %f, got %f", i, xi, tt.wantX[xi], x)
+			}
+			if x := expr.String(); x != tt.xstrings[xi] {
+				t.Errorf("%d: expected x[%d].String() = %s, got %s", i, xi, tt.xstrings[xi], x)
+			}
+			for xouti, xout := range expr.output {
+				if xout.String() != tt.xrpn[xi][xouti] {
+					t.Errorf("%d: expected x[%d].output[%d].String() = %s, got %s", i, xi, xouti, tt.xrpn[xi][xouti], xout.String())
+				}
 			}
 		}
 		names["Y"] = struct{}{}
